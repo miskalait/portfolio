ae098fbf721b547b6e0bcb9f0541dae466bfafd1
Miska Laitila <60491377+miskalait@users.noreply.github.com>
2022-11-01 12:15:43 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      'xxs': '300px',
+      'xs': '400px',
+      's': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
